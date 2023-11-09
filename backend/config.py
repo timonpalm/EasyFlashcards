5ca063416edf82879454d28de9545bc71c17a03f
timonpalm <timon.palm@gmx.de>
2023-11-09 15:15:15 +0100

    introduce config file

---
+++ backend/config.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = "" # Your HuggungFace API Token
+os.environ["OPENAI_API_KEY"] = "" # Your OpenAI API Key
+
+API_KEY = "" # Your API key to comunicate with frontend
